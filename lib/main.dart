d729c71202500eef0c1622382093cae5d7a63a1f
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2022-08-24 09:47:20 +0300

    release: v2.2.2

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'misc/url/url_strategy.dart';
 import 'features/settings/domain/theme_manager.dart';
 
 // TODO: Update before release
-const String appVersion = "v2.2.1";
+const String appVersion = "v2.2.2";
 
 void main() async {
   usePathUrlStrategy();
