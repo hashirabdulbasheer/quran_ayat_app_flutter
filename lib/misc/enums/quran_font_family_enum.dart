532ef401a9cf75dc50e700f37e9624b8759910c7
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2023-04-07 00:53:06 +0300

    config: updated arabic font file for quran

--- lib/misc/enums/quran_font_family_enum.dart
+++ lib/misc/enums/quran_font_family_enum.dart
@@ -1,11 +1,12 @@
 /// enum for fonts used by the app
 enum QuranFontFamily { arabic, malayalam }
 
+/// https://fonts.qurancomplex.gov.sa/wp02/en/%D8%AD%D9%81%D8%B5/
 extension QuranFontFamilyToString on QuranFontFamily {
   String get rawString {
     switch (this) {
       case QuranFontFamily.arabic:
-        return "KFGQPC Uthmanic Script HAFS Regular";
+        return "KFGQPC HAFS Uthmanic Script Regular";
       case QuranFontFamily.malayalam:
         return "Manjari Regular";
     }
