1501796156b018091887082b91460101047911c8
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2023-06-23 21:32:49 +0530

    Release v2.4.9

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 
 
 // TODO: Update before release
-const String appVersion = "v2.4.8";
+const String appVersion = "v2.4.9";
 
 void main() async {
   usePathUrlStrategy();
