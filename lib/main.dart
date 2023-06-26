0fdab5f6761ee5786a363ffed3ea8490ba816a92
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2023-06-26 23:10:05 +0530

    Release v2.5.0

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:redux/redux.dart';
 
 // TODO: Update before release
-const String appVersion = "v2.4.9";
+const String appVersion = "v2.5.0";
 
 void main() async {
   usePathUrlStrategy();
