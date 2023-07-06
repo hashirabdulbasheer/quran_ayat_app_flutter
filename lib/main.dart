06e072fdd082d63a1ee505f9f1711859986dfb2e
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2023-07-06 19:05:21 +0530

    Updated to v2.6.7

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ import 'package:redux/redux.dart';
 ///
 
 // TODO: Update before release
-const String appVersion = "v2.6.6";
+const String appVersion = "v2.6.7";
 
 void main() async {
   runZonedGuarded(
