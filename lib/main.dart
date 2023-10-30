a879b3f77edd73eab89347081b8fb6fed81e775a
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2023-10-30 08:20:27 +0300

    Updated to v2.7.3

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ import 'misc/url/url_strategy.dart';
 
 
 // TODO: Update before release
-const String appVersion = "v2.7.2";
+const String appVersion = "v2.7.3";
 
 void main() async {
   usePathUrlStrategy();
