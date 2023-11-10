6b96410e7949ece51fc3ae2d086268918dc2e43a
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2023-11-10 15:25:05 +0300

    Removed tooltip from each word in word by word aya

--- lib/features/ayats/presentation/widgets/ayat_display_word_by_word_widget.dart
+++ lib/features/ayats/presentation/widgets/ayat_display_word_by_word_widget.dart
@@ -44,80 +44,77 @@ class QuranAyatDisplayWordByWordWidget extends StatelessWidget {
                             enabled: true,
                             excludeSemantics: true,
                             container: true,
-                            child: Tooltip(
-                              message: '${e.ar} ${e.tr}',
-                              child: InkWell(
-                                onTap: () {
-                                  // TODO: Fix navigation to search
-                                  // if (_isInteractionAllowedOnScreen())
-                                  //   {
-                                  //     Navigator.push<void>(
-                                  //       context,
-                                  //       MaterialPageRoute(
-                                  //         builder: (context) =>
-                                  //             QuranSearchScreen(
-                                  //           searchString: e.ar,
-                                  //         ),
-                                  //       ),
-                                  //     ),
-                                  //   }
-                                  // else
-                                  //   {
-                                  //     _showMessage(
-                                  //       context,
-                                  //       QuranStrings.contPlayMessage,
-                                  //     ),
-                                  //   },
-                                },
-                                child: Padding(
-                                  padding: const EdgeInsets.only(
-                                    left: 8,
-                                    right: 8,
-                                  ),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.center,
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: <Widget>[
-                                      FittedBox(
-                                        fit: BoxFit.cover,
-                                        child: Text(
-                                          e.ar,
-                                          softWrap: false,
-                                          maxLines: 1,
-                                          textScaleFactor: fontScale,
-                                          style: TextStyle(
-                                            color: Colors.black,
-                                            fontSize: 30,
-                                            fontFamily: QuranFontFamily
-                                                .arabic.rawString,
-                                          ),
-                                          textAlign: TextAlign.center,
+                            child: InkWell(
+                              onTap: () {
+                                // TODO: Fix navigation to search
+                                // if (_isInteractionAllowedOnScreen())
+                                //   {
+                                //     Navigator.push<void>(
+                                //       context,
+                                //       MaterialPageRoute(
+                                //         builder: (context) =>
+                                //             QuranSearchScreen(
+                                //           searchString: e.ar,
+                                //         ),
+                                //       ),
+                                //     ),
+                                //   }
+                                // else
+                                //   {
+                                //     _showMessage(
+                                //       context,
+                                //       QuranStrings.contPlayMessage,
+                                //     ),
+                                //   },
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                  left: 8,
+                                  right: 8,
+                                ),
+                                child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    FittedBox(
+                                      fit: BoxFit.cover,
+                                      child: Text(
+                                        e.ar,
+                                        softWrap: false,
+                                        maxLines: 1,
+                                        textScaleFactor: fontScale,
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 30,
+                                          fontFamily: QuranFontFamily
+                                              .arabic.rawString,
+                                        ),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 2),
+                                    Container(
+                                      padding: const EdgeInsets.all(5),
+                                      decoration: const BoxDecoration(
+                                        border: Border.fromBorderSide(
+                                          BorderSide(color: Colors.black26),
+                                        ),
+                                        borderRadius: BorderRadius.all(
+                                          Radius.circular(1),
                                         ),
                                       ),
-                                      const SizedBox(height: 2),
-                                      Container(
-                                        padding: const EdgeInsets.all(5),
-                                        decoration: const BoxDecoration(
-                                          border: Border.fromBorderSide(
-                                            BorderSide(color: Colors.black26),
-                                          ),
-                                          borderRadius: BorderRadius.all(
-                                            Radius.circular(1),
-                                          ),
+                                      child: Text(
+                                        e.tr,
+                                        textScaleFactor: fontScale,
+                                        style: const TextStyle(
+                                          color: Colors.black54,
+                                          fontSize: 14,
                                         ),
-                                        child: Text(
-                                          e.tr,
-                                          textScaleFactor: fontScale,
-                                          style: const TextStyle(
-                                            color: Colors.black54,
-                                            fontSize: 14,
-                                          ),
-                                          textDirection: TextDirection.ltr,
-                                        ),
+                                        textDirection: TextDirection.ltr,
                                       ),
-                                    ],
-                                  ),
+                                    ),
+                                  ],
                                 ),
                               ),
                             ),
