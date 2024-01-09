0b12673226d9bb6e38d19f736d4b0af5c5a70898
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2024-01-09 18:58:37 +0300

    Added redux actions for challenge screen

---
+++ lib/features/challenge/domain/redux/actions/actions.dart
@@ -0,0 +1,7 @@
+import '../../../../core/domain/app_state/redux/actions/actions.dart';
+
+class InitializeChallengeScreenAction extends AppStateAction {}
+
+class NextChallengeScreenAction extends AppStateAction {}
+
+class PreviousChallengeScreenAction extends AppStateAction {}
