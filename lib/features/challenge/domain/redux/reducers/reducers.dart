e7aa5951ffafd3c3c0b54eb35d71afddbf1a5136
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2024-01-09 19:59:15 +0300

    Added redux reducers for challenge screen

---
+++ lib/features/challenge/domain/redux/reducers/reducers.dart
@@ -0,0 +1,48 @@
+import 'package:redux/redux.dart';
+
+import '../actions/actions.dart';
+import '../challenge_screen_state.dart';
+
+Reducer<ChallengeScreenState> readerScreenReducer =
+    combineReducers<ChallengeScreenState>([
+  TypedReducer<ChallengeScreenState, InitializeChallengeScreenAction>(
+    _initializeChallengeScreenReducer,
+  ),
+  TypedReducer<ChallengeScreenState, NextChallengeScreenAction>(
+    _nextChallengeScreenReducer,
+  ),
+  TypedReducer<ChallengeScreenState, PreviousChallengeScreenAction>(
+    _previousChallengeScreenReducer,
+  ),
+]);
+
+ChallengeScreenState _initializeChallengeScreenReducer(
+  ChallengeScreenState state,
+  InitializeChallengeScreenAction action,
+) {
+  return state;
+}
+
+ChallengeScreenState _nextChallengeScreenReducer(
+  ChallengeScreenState state,
+  NextChallengeScreenAction action,
+) {
+  int nextIndex = state.currentIndex + 1;
+  if (nextIndex < state.questions.length) {
+    return state.copyWith(currentIndex: nextIndex);
+  }
+
+  return state;
+}
+
+ChallengeScreenState _previousChallengeScreenReducer(
+  ChallengeScreenState state,
+  PreviousChallengeScreenAction action,
+) {
+  int previousIndex = state.currentIndex - 1;
+  if (previousIndex >= 0) {
+    return state.copyWith(currentIndex: previousIndex);
+  }
+
+  return state;
+}
