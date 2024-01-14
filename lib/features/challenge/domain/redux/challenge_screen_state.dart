0838feb5bc82947e64ad930a50c393f5693d3944
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2024-01-14 18:06:08 +0300

    Moved questions filtering logic to challend screen redux state

--- lib/features/challenge/domain/redux/challenge_screen_state.dart
+++ lib/features/challenge/domain/redux/challenge_screen_state.dart
@@ -1,6 +1,9 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:quran_ayat/features/challenge/domain/enums/quran_answer_status_enum.dart';
 
+import '../../../../models/qr_user_model.dart';
+import '../models/quran_answer.dart';
 import '../models/quran_question.dart';
 
 @immutable
@@ -34,6 +37,35 @@ class ChallengeScreenState extends Equatable {
   QuranQuestion? get currentQuestion => filteredQuestions.length > currentIndex
       ? filteredQuestions[currentIndex]
       : null;
+
+  // questions submitted by the logged in user
+  List<QuranQuestion> userSubmittedQuestions(QuranUser user) {
+    return _filterQuestionsBasedOn((element) => element.userId == user.uid);
+  }
+
+  // questions with approved answers
+  List<QuranQuestion> allApprovedAnswers(QuranUser user) {
+    return _filterQuestionsBasedOn((element) => element.status == QuranAnswerStatusEnum.approved);
+  }
+
+  List<QuranQuestion> _filterQuestionsBasedOn(bool Function(QuranAnswer element) answerCondition) {
+    List<QuranQuestion> questions = List.from(allQuestions);
+    List<QuranQuestion> filteredQuestions = [];
+    for (QuranQuestion question in questions) {
+      List<QuranAnswer> answers = question.answers.where((element) => answerCondition(element)).toList();
+      // latest on top
+      answers.sort((a, b,) => b.createdOn.compareTo(a.createdOn));
+      if(answers.isNotEmpty) {
+        filteredQuestions.add(
+          question.copyWith(answers: answers),
+        );
+      }
+    }
+    // latest on top
+    filteredQuestions.sort((a, b,) => b.createdOn.compareTo(a.createdOn));
+
+    return filteredQuestions;
+  }
 
   @override
   List<Object?> get props => [
