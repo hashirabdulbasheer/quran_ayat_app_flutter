f0a2c13ee75e83e7541038ad12ff47afc30e41bf
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2024-01-31 16:19:02 +0300

    Used homebottom sheet in new ayat screen

--- lib/features/newAyat/presentation/quran_new_ayat_screen.dart
+++ lib/features/newAyat/presentation/quran_new_ayat_screen.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-import 'package:quran_ayat/features/bookmark/domain/bookmarks_manager.dart';
-import 'package:quran_ayat/features/newAyat/data/surah_index.dart';
 import 'package:redux/redux.dart';
 
 import '../../bookmark/data/bookmarks_local_impl.dart';
+import '../../bookmark/domain/bookmarks_manager.dart';
 import '../../bookmark/presentation/bookmark_icon_widget.dart';
 import '../../core/domain/app_state/app_state.dart';
 import '../../drawer/presentation/nav_drawer.dart';
-import '../../settings/domain/theme_manager.dart';
+import '../../home/presentation/quran_home_screen.dart';
+import '../../home/presentation/widgets/quran_home_bottom_sheet_widget.dart';
+import '../data/surah_index.dart';
 import '../domain/redux/actions/actions.dart';
 import 'quran_new_ayat_widget.dart';
 
@@ -43,57 +44,9 @@ class _QuranNewAyatScreenState extends State<QuranNewAyatScreen> {
             ),
           ),
           // onDrawerChanged: null,
-          bottomSheet: Padding(
-            padding: const EdgeInsets.fromLTRB(
-              10,
-              10,
-              10,
-              30,
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: Tooltip(
-                        message: "Previous aya",
-                        child: ElevatedButton(
-                          style: _elevatedButtonTheme,
-                          onPressed: () => _moveToPreviousAyat(
-                            store,
-                          ),
-                          child: Icon(
-                            Icons.arrow_back,
-                            color: _elevatedButtonIconColor(
-                              context,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 10),
-                    Expanded(
-                      child: Tooltip(
-                        message: "Next aya",
-                        child: ElevatedButton(
-                          style: _elevatedButtonTheme,
-                          onPressed: () => _moveToNextAyat(
-                            store,
-                          ),
-                          child: Icon(
-                            Icons.arrow_forward,
-                            color: _elevatedButtonIconColor(
-                              context,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+          bottomSheet: QuranHomeBottomSheetWidget(
+            store: store,
+            selectedTab: QuranHomeScreenBottomTabsEnum.reader,
           ),
           appBar: AppBar(
             centerTitle: true,
@@ -118,53 +71,5 @@ class _QuranNewAyatScreenState extends State<QuranNewAyatScreen> {
         ),
       );
     });
-  }
-
-  /// display next aya
-  void _moveToNextAyat(
-    Store<AppState> store,
-  ) {
-    store.dispatch(NextAyaAction());
-  }
-
-  /// display previous aya
-  void _moveToPreviousAyat(
-    Store<AppState> store,
-  ) {
-    store.dispatch(PreviousAyaAction());
-  }
-
-  ///
-  /// Theme
-  ///
-
-  ButtonStyle? get _elevatedButtonTheme {
-    // if system dark mode is set then use dark mode buttons
-    // else use gray button
-    if (QuranThemeManager.instance.isDarkMode()) {
-      return ElevatedButton.styleFrom(
-        backgroundColor: Colors.white70,
-        shadowColor: Colors.transparent,
-        textStyle: const TextStyle(color: Colors.black),
-      );
-    }
-
-    return ElevatedButton.styleFrom(
-      backgroundColor: Colors.black12,
-      shadowColor: Colors.transparent,
-      textStyle: const TextStyle(color: Colors.deepPurple),
-    );
-  }
-
-  Color? _elevatedButtonIconColor(
-    BuildContext context,
-  ) {
-    // if system dark mode is set then use dark mode buttons
-    // else use primate color
-    if (QuranThemeManager.instance.isDarkMode()) {
-      return null;
-    }
-
-    return Theme.of(context).primaryColor;
   }
 }
---
+++ lib/misc/design/design_system.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+/// Design System
+class QuranDS {
+  /// TextSTyle
+  static TextStyle textTitleSmall = const TextStyle(fontSize: 12);
+
+  /// ButtonStyle
+  static ButtonStyle elevatedButtonTheme = ElevatedButton.styleFrom(
+    backgroundColor: Colors.white70,
+    shadowColor: Colors.transparent,
+    textStyle: const TextStyle(color: Colors.black),
+  );
+}
