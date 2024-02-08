8021f70ddd84bb224747813d7f9b5cc32cad448f
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2024-02-08 17:27:32 +0300

    v3.1.1

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'misc/enums/quran_feature_flag_enum.dart';
 import 'misc/url/url_strategy.dart';
 
 // TODO: Update before release
-const String appVersion = "v3.1.0";
+const String appVersion = "v3.1.1";
 
 bool isChallengeBetaModeEnabled = RemoteConfigManager.instance
         .get(RemoteConfigFeatureFlagEnum.isChallengeScreenEnabled);
